34f917ba7911ef84f3fa526ba6888313a0ac75f6
Callum Rogers <callumrogers@hotmail.co.uk>
2012-04-06 16:10:31 +0100

    Added Arm.cs

---
+++ RobotControllerLib/Arm.cs
@@ -0,0 +1,22 @@
+﻿using NKH.MindSqualls;
+
+namespace RobotControllerLib
+{
+    class Arm
+    {
+        public Joint ShoulderTwist { get; private set; }
+        public Joint ShoulderExtend { get; private set; }
+
+        public Joint ElbowTwist { get; private set; }
+        public Joint ElbowExtend { get; private set; }
+
+
+        public Arm(NxtMotor shoulderTwistMotor, NxtMotor shoulderExtendMotor, NxtMotor elbowTwistMotor, NxtMotor elbowExtendMotor)
+        {
+            ShoulderTwist = new Joint(shoulderTwistMotor);
+            ShoulderExtend = new Joint(shoulderExtendMotor);
+            ElbowTwist = new Joint(elbowTwistMotor);
+            ElbowExtend = new Joint(elbowExtendMotor);
+        }
+    }
+}
