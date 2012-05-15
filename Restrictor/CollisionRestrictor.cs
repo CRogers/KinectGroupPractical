d61874c3ac2824fcd7c95a2fe2e60acb414433f4
michaelbradley91 <michael.bradley@hotmail.co.uk>
2012-05-15 17:11:50 +0100

    Collision Restrictor partially fixed

--- Restrictor/CollisionRestrictor.cs
+++ Restrictor/CollisionRestrictor.cs
@@ -109,6 +109,8 @@ namespace Restrictor
         //Tell the class to try setting the real robot's angles.
         public void commitAngles(AnglePositions angles)
         {
+
+            kinectRobot.setAngles(angles); //for display purposes
             if (isRobotReady())
             {
                 robot.SetAngles(angles);
@@ -118,7 +120,7 @@ namespace Restrictor
         //For sending in data from the Kinect
         public void kinectDataIn(AnglePositions angles)
         {
-            kinectRobot.setAngles(angles); //for display purposes
+            
             foreach (var listener in listeners)
             {
                 listener.kinectAngles(angles);
